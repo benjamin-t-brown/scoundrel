d9e6b59f7606905b3532212886d4f059c02d095c
benjamin-t-brown <benjamin.t.brown@hotmail.com>
2025-03-09 21:10:51 -0600

    create gh-pages

--- miyooa30-toolchain/example/hello-sdl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /mnt/SDCARD/hello_sdl2
-exec /mnt/SDCARD/hello_sdl2/hello_sdl2
--- src/LibHTML.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace libhtml {
-void notifyGameStarted();
-void notifyGameReady();
-void notifyGameCompleted(bool isVictory);
-} // namespace libhtml
